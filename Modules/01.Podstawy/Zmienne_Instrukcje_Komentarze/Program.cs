61f1dfcc49c9975a2add9896ebd8521ca6c7f489
kazu1121 <adamkazmiers@gmail.com>
2015-09-02 23:01:49 +0200

    [New] Stworzenie projektu Zmienne_Instrukcje_Komentarze.

---
+++ Modules/01.Podstawy/Zmienne_Instrukcje_Komentarze/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmienne_Instrukcje_Komentarze
+{
+    class Program
+    {
+        // Miejsce startu programu.
+        static void Main(string[] args)
+        {
+            // ZMIENNE LOKALNE - POJEMNIKI NA DANE.
+
+            // Deklaracja zmiennej typu int.
+            int calkowita;
+
+            // Inicjalizacja zadeklarowanej zmiennej.
+            calkowita = 12;
+
+            // Deklaracja z inicjalizacją.
+            double zmiennoprzecinkowa = 123.3;
+
+            // Niejawne określenie typu danych.
+            var dowolny = "Każdy typ, ale trzeba uważać!";
+
+            var zFloat = 123.12f;
+
+            var zDecimal = 321.12345;
+
+
+            // INSTRUKCJE - WYKORZYSTYWANIE DANYCH.
+
+            // Instrukcja deklaracji i inicjalizacji (przypisania).
+            bool takCzyNie = false;
+
+
+            // Instrukcje wyrażeń - Sekwencja operatorów i operandów.
+
+            int wynik = 2 + 2*(2 - 4) + 8;
+
+            // 2 + 2 to Samo wyrażenie, ale int wynik2 = 2 + 2; to instrukcja wyrażenia.
+
+            Console.WriteLine("Hello World");
+
+            bool czyRowne = 2 == 2;
+            
+            // Operatory mają swoje priorytety
+
+
+            Console.ReadKey();
+        }
+    }
+}
