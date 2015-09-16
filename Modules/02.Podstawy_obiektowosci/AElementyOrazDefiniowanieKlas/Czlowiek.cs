ae3ebfdb0771f35097eb7e54a9822b712cf20bd2
kazu1121 <adamkazmiers@gmail.com>
2015-09-16 11:45:13 +0200

    [New] Rozpoczęcie modułu 02.Podstawy_obiektowosci. Dodanie  nowego projektu AElementyOrazDefiniowanieKlas.

---
+++ Modules/02.Podstawy_obiektowosci/AElementyOrazDefiniowanieKlas/Czlowiek.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AElementyOrazDefiniowanieKlas
+{
+    public class Czlowiek
+    {
+        // namespace -> class -> składowe klasy 
+
+        // Właściwości - reprezentowanie stanu obiektu
+
+        public string Imie { get; set; }
+        private string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+
+        public Czlowiek()
+        {
+            // Konstruktor pozwala nam dokonać "konfiguracji" podczas tworzenia obiektów
+            // Tak wygląda domyślny konstruktor - tylko tworzy obiekt
+        }
+
+
+        // Konstruktor klasy Czlowiek
+        public Czlowiek(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+
+        // Konstruktorów może być wiele
+        public Czlowiek(string imie, string nazwisko, DateTime dataUrodzenia)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            DataUrodzenia = dataUrodzenia;
+        }
+
+
+        public void Przedstaw()// metoda klasy Czlowiek
+        {
+            Console.WriteLine("{0} {1} ur. {2: yyyy/MM/dd}", Imie, Nazwisko, DataUrodzenia);
+        }
+    }
+}
---
+++ Modules/02.Podstawy_obiektowosci/AElementyOrazDefiniowanieKlas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AElementyOrazDefiniowanieKlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek obj1 = new Czlowiek();// Stworzenie obiektu/instancji klasy
+
+            obj1.Imie = "Jan";
+            // obj1.Nazwisko = "Kowalski";
+            obj1.DataUrodzenia = new DateTime(1994, 3, 27);
+
+            obj1.Przedstaw();
+
+
+            Console.ReadKey();
+        }
+    }
+}
