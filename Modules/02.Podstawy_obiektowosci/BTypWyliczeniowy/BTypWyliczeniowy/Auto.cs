d482875ec6c1ec9f6e46fedd7d05ac57796d1aca
kazu1121 <adamkazmiers@gmail.com>
2015-09-20 16:22:45 +0200

    [New] Dodanie projektu BTypWyliczeniowy

---
+++ Modules/02.Podstawy_obiektowosci/BTypWyliczeniowy/BTypWyliczeniowy/Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTypWyliczeniowy
+{
+    // Typ wyliczeniowy - powiązane ze sobą stałe
+
+    enum Marka
+    {
+        Ferrari = 2, // Domyślnie ma wartość 0
+        Mercedes // Przyjmuje poprzednią wartość powiększoną o 1
+    }
+
+
+    class Auto
+    {
+        // Full property
+        private Marka _markaAuta;
+
+        public Marka MarkaAuta
+        {
+            get
+            {
+                return _markaAuta;
+            }
+            set
+            {
+                _markaAuta = value;
+            }
+        }
+    }
+}
---
+++ Modules/02.Podstawy_obiektowosci/BTypWyliczeniowy/BTypWyliczeniowy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTypWyliczeniowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto();
+
+            // Przykład przypisania wartości do właściwości typu wyliczeniowego
+            auto1.MarkaAuta = Marka.Ferrari; 
+            Console.WriteLine("\nWartość marki {0} w typie wyliczeniowym wynosi {1}",
+                              auto1.MarkaAuta, (int)auto1.MarkaAuta);
+
+
+            auto1.MarkaAuta = Marka.Mercedes;
+            Console.WriteLine("\nWartość marki {0} w typie wyliczeniowym wynosi {1}",
+                              auto1.MarkaAuta, (int)auto1.MarkaAuta);
+
+            Console.ReadKey();
+        }
+    }
+}
